48bd94c8f6e471bcf6ecb125651d665df95464c8
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-25 14:12:43 +0530

    save totalwage for each company

---
+++ save.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementt
+{
+    public class save
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHourPerMonth;
+        private int totalEmpWage;
+
+        public save(string company, int empRatePerHour, int numOfWorkingDays, int maxHourPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHourPerMonth = maxHourPerMonth;
+        }
+
+        public void computeEmpWage()
+        {
+            //variables
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= this.maxHourPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("DAY#:" + totalWorkingDays + "Emp Hrs:" + empHrs);
+            }
+            totalEmpWage = totalEmpHrs * this.empRatePerHour;
+            Console.WriteLine("total emp wage for company:" + company + "is:" + totalEmpWage);
+        }
+        public string toString()
+        {
+            return "total emp wage for company:" + this.company + "is:" + this.totalEmpWage;
+        }
+
+    }
+}
